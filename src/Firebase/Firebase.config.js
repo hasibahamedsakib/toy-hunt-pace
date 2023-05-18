32354b3c35adbb45cbae9424fa58f93bdddc9f79
Hasib-Ahamed <hasibahamedsakib1@gamil.com>
2023-05-18 11:03:21 +0600

    added login page

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcYqeXYjFSwv7bktA2fBFAe5IvnZPivtw",
+  authDomain: "toy-hunt-place.firebaseapp.com",
+  projectId: "toy-hunt-place",
+  storageBucket: "toy-hunt-place.appspot.com",
+  messagingSenderId: "1038432578874",
+  appId: "1:1038432578874:web:303345fcf294cd216b7403",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,70 @@
+import { Button, Checkbox, Label, TextInput } from "flowbite-react";
+
 const Login = () => {
+  const logo = "https://i.ibb.co/wYD5mjJ/R-4-removebg-preview.png";
   return (
-    <div>
-      <h1>Login page</h1>
+    <div className="bg-slate-400 ">
+      <div className=" container ">
+        <div className="w-60 text-center  mx-auto">
+          <img src={logo} className=" mx-auto w-28 h-28" alt="toy hunt logo" />
+        </div>
+        <div className="w-[600px] bg-white mx-auto p-10 rounded-lg">
+          <form className="flex flex-col gap-4 ">
+            <div className="text-center text-slate-500 space-y-2">
+              <h2 className="text-4xl font-bold  ">Sign In</h2>
+              <hr />
+              <p>Enter your e-mail address and your password</p>
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label color="light" htmlFor="email1" value="Your email" />
+              </div>
+              <TextInput
+                sizing="md"
+                color="light"
+                id="email1"
+                type="email"
+                name="email"
+                placeholder="email@example.com"
+                required={true}
+              />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label
+                  color="light"
+                  htmlFor="password1"
+                  value="Your password"
+                />
+              </div>
+              <TextInput
+                sizing="md"
+                color="light"
+                id="password1"
+                name="password"
+                type="password"
+                required={true}
+              />
+            </div>
+            <div className="flex items-center gap-2">
+              <Checkbox
+                className="text-slate-500 focus:outline-slate-500 hover:border-none"
+                id="remember"
+              />
+
+              <Label htmlFor="remember">Remember me</Label>
+            </div>
+            <Button
+              size="lg"
+              //   gradientDuoTone="pinkToOrange"
+              className="bg-gray-700 hover:bg-slate-600"
+              type="submit"
+            >
+              login
+            </Button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
